5856518ebe3398ded2592b4e064f6f2ea863764b
Willmer Gonzalez <willmer2911@gmail.com>
2017-08-04 16:01:48 -0400

    aplicando animaciones los primeros pasos

--- js/scroll-animado.js
+++ js/scroll-animado.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
+
 	$('a[href*="#"]:not([href="#"])').click(function() {
+
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
 			if (target.length) {
 				$('html, body').animate({
 					scrollTop: target.offset().top
 				}, 1000);
+
 				return false;
 			}
 		}
 	});
+
+	// ejemplo: http://blog.grayghostvisuals.com/js/detecting-scroll-position/
+	var inicio = $('#inicio').height();
+	console.log('Voy 1: ' + inicio);
+
+	function Activar_Animacion() {
+		console.log('Voy 2');
+		var scroll_top = $(window).scrollTop();
+
+		if (scroll_top >= inicio) {
+			$('.entrada').addClass('.fadeInUp.animated');
+			console.log('carajo');
+		} else {}
+	}
+
+	$(window).scroll(Activar_Animacion);
+	console.log('Voy 3');
 });
